b9553abab43c66ccd6fcac07b751d462b1c35750
cksajil <sajilck@gmail.com>
2019-06-22 11:46:37 -0400

    Add Resolution Change

--- readCam.py
+++ readCam.py
@@ -1,18 +1,26 @@
 # import Libraries
 import numpy as np
+from SetResolution import ChangeRes
 import cv2
 
 # Initialize Device
-cap = cv2.VideoCapture(1)
+cam 		= 		cv2.VideoCapture(1)
+
+
+# Set Resolution
+# cam 		= 		ChangeRes(cam, 1280, 720)
+cam 		= 		ChangeRes(cam, 1944, 2592)
 
 
 # Start reading frames
 while(True):
-    ret, frame = cap.read()
+    ret, frame = cam.read()
+
     cv2.imshow('frame',frame)
+    print(frame.shape)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release Device
-cap.release()
+cam.release()
 cv2.destroyAllWindows()
