08d337a7985bb615a2056cde6980a123a4f18a5a
cksajil <sajilck@gmail.com>
2019-06-22 11:01:50 -0400

    Add Comments

--- readCam.py
+++ readCam.py
@@ -1,13 +1,18 @@
+# import Libraries
 import numpy as np
 import cv2
 
+# Initialize Device
 cap = cv2.VideoCapture(1)
 
+
+# Start reading frames
 while(True):
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# Release Device
 cap.release()
 cv2.destroyAllWindows()
